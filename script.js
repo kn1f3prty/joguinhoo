f3b5e3339d6442acf9450cb858768cde71631a71
ma ☆ <114525589+kn1f3prty@users.noreply.github.com>
2024-08-08 07:50:50 -0300

    fdgd

--- script.js
+++ script.js
@@ -6,6 +6,16 @@ const caixaResultadol = document.querySelector(".caixa-resultado");
 
 const perguntas = [
     {
-        enunciado: "No âmbito social, você prefere:"
+        enunciado: "No âmbito social, você prefere:",
+        alternativas: ["Acabar com todos os problemas sociais do Brasil, como a pobreza, fome, preconceitos raciais, etc...mas extinguir a amazonia.", "Todas as crianças no sistema de adoçao encontrariam um bom lar, mas... a quantidade de animais abandonados duplicaria.
+"]
+    },
+    {
+         enunciado: "No âmbito ambiental, você prefere:",
+        alternativas: ["Alternativa 3", "Alternativa 4"
+    },
+    {
+        enunciado: "No âmbito tecnológico, você prefere:",
+        alternativas: ["Alternativa 5", "Alternativa 6"
     }
 ]
