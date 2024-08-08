4aa60d70a52a3f99f78f6dd65f4d063f893e0182
ma ☆ <114525589+kn1f3prty@users.noreply.github.com>
2024-08-08 07:42:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+const caixaPrincipal = documment.querySelector(".caixa-principal");
+const caixaPerguntas = documment.querySelector(".caixa-perguntas");
+const caixaAlternativas = documment.querySelector(".caixa-alternaivas");
+const caixaResultados = documment.querySelector(".caixa-resultado");
+const caixaResultadol = documment.querySelector(".caixa-resultado");
