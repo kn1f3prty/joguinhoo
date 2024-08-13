67ab0f91f784d038fade1f4e2231e2a6d747b69c
ma ☆ <114525589+kn1f3prty@users.noreply.github.com>
2024-08-13 08:43:45 -0300

    wr5wr

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const perguntas = [
         alternativas: [
             {
                  texto: "Acabar com todos os problemas sociais do Brasil, como a pobreza, fome, preconceitos raciais, etc...mas extinguir a Amazônia.",
-                 afirmacao: "Parabéns! Você acabou com todos os probleas sociais do país! ...Agora imagine a Amazônia queimando e todos ops animaizinhos morrendo em agonia.",
+                 afirmacao: "Parabéns! Você acabou com todos os probleas sociais do país! ...Agora imagine a Amazônia queimando e todos os animaizinhos morrendo em agônia. <3 ",
 
             },
             {
