d470d0babb1733918ac42cb4550317f5eaa24401
ma ☆ <114525589+kn1f3prty@users.noreply.github.com>
2024-08-20 08:33:16 -0300

    dddd

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultados = document.querySelector(".caixa-resultado");
-const caixaResultadol = document.querySelector(".caixa-resultado");
+const  textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
